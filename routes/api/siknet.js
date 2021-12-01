3c041214f3c8bd8e2861103a80831fd9459074ba
Qahtan <qahtansaidmartak@gmail.com>
2021-12-01 11:39:35 +0700

    add data kamar

--- routes/api/siknet.js
+++ routes/api/siknet.js
@@ -1,13 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-// const {
-//   jadwalDokter, listDokter,
-// } = require("../../controllers/api/doctorController");
-// const { getJadwal } = require("../../helpers/validators/doctor");
-
-// router.get("/jadwal", getJadwal, jadwalDokter);
-// router.get("/list", listDokter);
 
 router.get("/list", list);
 
--- routes/api/simrs.js
+++ routes/api/simrs.js
@@ -6,7 +6,29 @@ const {
 // const { getJadwal } = require("../../helpers/validators/doctor");
 
 // router.get("/data_kamar", getJadwal, jadwalDokter);
+
 router.get("/data_kamar", listDatakamar);
+
+
+/**
+ * @swagger
+ * /simrs/data_kamar:
+ *  get:
+ *    summary: Lihat data kamar
+ *    responses:
+ *      200:
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Response'
+ *      400:
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Response'
+ *              
+ *              
+ */
 
 
 
