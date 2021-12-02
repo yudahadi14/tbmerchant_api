de5efbe187fa65c6ef0f34589cb09afa16d1b2f4
Qahtan <qahtansaidmartak@gmail.com>
2021-12-02 15:46:00 +0700

    change port

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.use(function (req, res, next) {
 });
 
 if (process.env.NODE_ENV === "production") {
-  port = 2001;
+  port = 5001;
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
