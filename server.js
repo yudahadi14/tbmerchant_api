d830dd567a9bd1d1dd4ecf0490a3e5a7198b97c4
yudahadi14 <yudahadi14@gmail.com>
2023-03-05 13:14:50 +0700

    y

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const cors = require("cors");
 const path = require("path");
 const dotenv = require("dotenv");
 dotenv.config();
-const routeDoctor = require("./routes/api/doctor");
-const apiAuth = require("./routes/api/auth");
-const apiLayanan = require("./routes/api/layanan");
-const apiMaster = require("./routes/api/master");
-const apiLab = require("./routes/api/lab");
-const apiRad = require("./routes/api/rad");
+// const routeDoctor = require("./routes/api/doctor");
+// const apiAuth = require("./routes/api/auth");
+// const apiLayanan = require("./routes/api/layanan");
+// const apiMaster = require("./routes/api/master");
+// const apiLab = require("./routes/api/lab");
+// const apiRad = require("./routes/api/rad");
 const swaggerUi = require("swagger-ui-express");
 const swaggerJsDoc = require("swagger-jsdoc");
 const bodyParser = require("body-parser");
-const models = require("./models");
+// const models = require("./models");
 // const swaggerDocument = require('./swagger.json');
 const options = {
   definition: {
@@ -20,21 +20,21 @@ const options = {
     info: {
       title: "Library API",
       version: "1.0.0",
-      description: "RSUDC API",
+      description: "TOLONG BELIIN API",
     },
     servers: [
       {
         url:
           process.env.NODE_ENV === "production"
-            ? "http://api.rsudcengkareng.com:5001/api"
-            : "http://172.18.38.164:5001/api",
+            ? ""
+            : "http://127.0.0.1:5001/api",
       },
     ],
   },
   apis: ["./routes/api/*.js"],
 };
 const specs = swaggerJsDoc(options);
-const routeSimrs = require("./routes/api/simrs");
+// const routeSimrs = require("./routes/api/simrs");
 const auth = require("./middleware/authMid");
 const base64ToFile = require("./helpers/utility/base64ToFile");
 // const routeService = require("./routes/api/service");
@@ -62,13 +62,13 @@ app.options("*", cors());
 app.use(bodyParser.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 app.use(express.json());
-app.use("/api/doctor", routeDoctor);
-app.use("/api/simrs", routeSimrs);
-app.use("/api/auth", apiAuth);
-app.use("/api/layanan", apiLayanan);
-app.use("/api/master", apiMaster);
-app.use("/api/lab", apiLab);
-app.use("/api/rad", apiRad);
+// app.use("/api/doctor", routeDoctor);
+// app.use("/api/simrs", routeSimrs);
+// app.use("/api/auth", apiAuth);
+// app.use("/api/layanan", apiLayanan);
+// app.use("/api/master", apiMaster);
+// app.use("/api/lab", apiLab);
+// app.use("/api/rad", apiRad);
 
 //SWAGGER
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
@@ -92,25 +92,43 @@ app.listen(port, () => {
 });
 // sequelize-auto -h 192.168.200.200 -d rsudc -u admin -x admin   --dialect postgres -c ./config/config.js -o ./models -t billing_va_dki
 
-models.sequelize
-  // .sync()
-  .authenticate()
-  .then((aw) => {
-    console.log({
-      env: process.env.NODE_ENV,
-      dbName: models.sequelize.getDatabaseName(),
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-let whereAddOn = {
-  ref_prod_nama: ["Vaksinasi Meningitis", "Vaksinasi Influenza"],
-};
-
-console.log({
-  where: {
-    ...whereAddOn,
-  },
+// models.sequelize
+//   // .sync()
+//   .authenticate()
+//   .then((aw) => {
+//     console.log({
+//       env: process.env.NODE_ENV,
+//       dbName: models.sequelize.getDatabaseName(),
+//     });
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+const mysql = require('mysql');
+var connectionn = mysql.createConnection({
+  host: "tbi-db.cj6xy2r6pcji.ap-southeast-3.rds.amazonaws.com",
+  user: "admin",
+  password: "rDPWrEPniw9gOXi0AZbp",
+  port: "3306",
+  database: "tb_merchant"
 });
+
+connectionn.connect(function (err){
+  if(err)
+  {
+    console.log('Database Connection Error' + err.stack);
+  }else{
+    console.log('Connect to Database');
+  }
+  
+});
+
+// let whereAddOn = {
+//   ref_prod_nama: ["Vaksinasi Meningitis", "Vaksinasi Influenza"],
+// };
+
+// console.log({
+//   where: {
+//     ...whereAddOn,
+//   },
+// });
